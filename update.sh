c0a923570640aa616055c270dcaf86013eb79afd
Ruslan Vostretsov <ElectricalBrain@protonmail.com>
2022-07-24 14:45:53 +0300

    Automate update

--- update.sh
+++ update.sh
@@ -1,12 +1,43 @@
 #!/bin/env bash
 
 
-dot_files=(".profile" ".bashrc" ".aliases" ".bash_profile" ".bash_aliases" ".emacs.d/" ".gitconfig" ".selected_editor" ".tmux.conf.local")
+dot_files=(".profile" ".bashrc" ".aliases" ".bash_profile" ".bash_aliases" ".gitconfig" ".selected_editor" ".tmux.conf.local")
+dot_dirs=(".emacs.d/")
 
 
-## create sym link to connect dot files
-for dotfile in "${dot_files[@]}"; do
-    cp -r "$HOME/$dotfile" "./$dotfile"
-done
+function update_repo() {
+    echo "Updating repo..."
+    for dotfile in "${dot_files[@]}"; do
+	cp "$HOME/$dotfile" "./$dotfile"
+    done
+    for dotdir in "${dot_dirs[@]}"; do
+	cp -r "$HOME/$dotdir" "./$dotdir"
+    done
+}
+
+function update_user() {
+    echo "Updating user files..."
+    for dotfile in "${dot_files[@]}"; do
+	cp "./$dotfile" "$HOME/$dotfile"
+    done
+}
+
+function usage() {
+    echo "usage:"
+    echo "    $0 repo - update repo"
+    echo "    $0 user - update user files"
+}
+
+
+
+if [ "$1" = "repo" ]; then
+    update_repo
+elif [ "$1" = "user" ]; then
+    update_user
+else
+    echo "$0: Wrong usage"
+    usage
+fi
 
 echo done
+exit 0
